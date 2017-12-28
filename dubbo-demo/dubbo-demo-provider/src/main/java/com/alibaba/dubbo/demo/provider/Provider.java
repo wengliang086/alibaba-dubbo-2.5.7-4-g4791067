681e104b50b1347ba351248efa8ba0ddc9d646f6
nangua <nangua.com>
2017-12-28 13:27:47 +0800

    加注释

--- dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
+++ dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
@@ -8,6 +8,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Provider {
 
     public static void main(String[] args) throws Exception {
+    	/**
+    	 * DubboNamespaceHandler 加载 ServiceBean（实现了 ApplicationListener 接口）
+    	 * Spring 提供的 ApplicationListener 会在容器初始化完成之后调用 onApplicationEvent 方法
+    	 * onApplicationEvent 方法中调用 ServiceConfig 类中的 export() 方法启动开始
+    	 */
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
         context.start();
 
